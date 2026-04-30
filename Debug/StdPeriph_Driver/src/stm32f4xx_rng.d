24030712d7c72b528bf55b6353072faf395b8049
Alejo Hernandez <alejohk13@gmail.com>
2026-04-30 10:57:37 -0300

    Comienzo de proyecto (nada de codigo en Eclipse realizado)

---
+++ Debug/StdPeriph_Driver/src/stm32f4xx_rng.d
@@ -0,0 +1,104 @@
+StdPeriph_Driver/src/stm32f4xx_rng.o: \
+ ../StdPeriph_Driver/src/stm32f4xx_rng.c \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/device/stm32f4xx.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cm4.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cmInstr.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cmFunc.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cmSimd.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/misc.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rcc.h
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/device/stm32f4xx.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cm4.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cmInstr.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cmFunc.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/core/core_cmSimd.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/Usuario/workspace/TP2_AÑO_Hernandez/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
